e97d2d7a8d1af19b26c52f19c9d76d6b106ab79e
develon2015 <302615249@qq.com>
2023-09-08 18:16:33 +0800

    Promise

---
+++ app/src/main/kotlin/open/kotlin/Promise.kt
@@ -0,0 +1,28 @@
+package open.kotlin
+
+import kotlin.coroutines.*
+import log4kt.log
+
+/**
+ * Continuation 可以看作是一种 Promise，resume、resumeWith、resumeWithException等方法提供了 resolve 和 reject
+ */
+suspend fun getPromise(world: String? = null): String =
+        suspendCoroutine<String> { continuation: Continuation<String> ->
+            if (world == null) {
+                continuation.resumeWithException(RuntimeException("Damn it!"))
+            } else {
+                continuation.resume("Hello, " + world)
+            }
+        }
+
+fun promise(): suspend () -> Any {
+    return suspend {
+        val hi = getPromise("World")
+        log.d("hi = $hi")
+        try {
+            getPromise()
+        } catch (e: Throwable) {
+            log.e("getPromise(null) =", e)
+        }
+    }
+}
